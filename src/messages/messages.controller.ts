32a26d8422f5a4c1e1b933873407aed72b719623
Rafay Ahmed <rafayahmed3003@gmail.com>
2023-12-11 07:10:06 -0800

    Message Controller updated with Message Services

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,22 +1,30 @@
 import { Controller , Get , Post , Body , Param} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+    messageService: MessagesService;
+
+    constructor(){
+        //Service is creating it's own dependency
+        //Don't do it in Real Apps
+        this.messageService = new MessagesService();
+    }
     
     @Get()
     listMessage(){
-
+        return this.messageService.findAll();
     }
 
     @Post()
     createMessage(@Body() body: CreateMessageDto){
-    console.log(body)
+        return this.messageService.create(body.content);
     }
 
     @Get('/:id')
     getMessage(@Param('id') id: string){
-console.log(id)
+        return this.messageService.findOne(id);
     }
 
 }
